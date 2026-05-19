c2414729333bff922b2b3e88c4d3ba9e9fd0d189
Alec Ours <a.ours1999@gmail.com>
2026-05-18 20:50:54 -0700

    Add logic for vertical wins

--- script.js
+++ script.js
@@ -27,42 +27,51 @@ function newPlayer(name, marker) {
     // when p1 turn, put down p1 marker
         // if p2 turn, put down p2 marker
     
-    gameBoard[0][2] = player1.marker;
-    gameBoard[1][1] = player1.marker;
+    gameBoard[0][0] = player1.marker;
+    gameBoard[1][0] = player1.marker;
     gameBoard[2][0] = player1.marker;
 
-    for (let i = 0; i < 9; i++) {
-            if (gameBoard[1][1] = "X") {
-                if (checkDiagonalWin() === true) {
+    for (let col = 0; col < 3; col++) {
+        for (let row = 0; row < 3; row++) {
+            if (gameBoard[row][0] === "X") {
+                if (checkVerticalWin(row) === true) {
                     console.log("Winner!");
+                    p1Win = true;
                     break;
                 }
+            };
+
+            // if gameBoard[n][0] contains "X", check horizontal win    
+
+            if (gameBoard[1][1] === "X") {
+                if (checkDiagonalWin() === true) {
+                    console.log("Winner!");
+                    p1Win = true;
+                    break;
+                };
+            };
         };
     };
 
-    // if gameBoard[n][0] contains "X", check horizontal win
-
-    // if gameBoard[0][n] contains "X", check vertical win
+    function checkVerticalWin(row) {
+        if (gameBoard[row+1][0] === "X" && gameBoard[row+2][0] === "X") {
+            return true;
+        }
+        else {
+            return false;
+        };
+    }
 
     // horizonal win
         // if gameBoard[n][0+1] and gameBoard[n][0+2] = "X", win
 
-    // vertical win
-        // if gameBoard[0+1][n] and gameBoard[0+2][n] = "X", win 
-    
     function checkDiagonalWin() {
         if (gameBoard[0][0] === "X" && gameBoard[0+2][0+2] === "X") {
             return true;
-            console.log("p1Win = " + p1Win);
         }; 
 
         if (gameBoard[0][+2] === "X" && gameBoard[0+2][0] === "X") {
             return true;
-            console.log("p1Win = " + p1Win);
         };
-    };
-    // diagonal win
-        // if gameBoard [0+1][0+1] = "X", 
-            // if gameBoard[0][0] = "X", and gameBoard[0+2][0+2] = X, win.
-            // if gameBoard[0][+2] = "X", and gameBoard[0+2][0] = "X", win.
+    }
 })();
